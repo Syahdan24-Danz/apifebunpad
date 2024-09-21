88591e0434fe148cf41a412a8afd7c0449cf59ef
syahdanf860 <syahdan22001@mail.unpad.ac.id>
2024-09-22 06:40:50 +0700

    menambah env

--- connection.js
+++ connection.js
@@ -8,10 +8,10 @@ const mysql = require("mysql");
 // });
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "db_bemfeb",
+  host: "api.bemfebunpad.my.id",
+  user: "bemfebun_admin",
+  password: "Cc,yzEXQenK9",
+  database: "bemfebun_db_bemfeb",
 });
 
 db.connect((err) => {
